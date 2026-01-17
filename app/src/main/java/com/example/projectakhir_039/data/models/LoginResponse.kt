b7b6dc3a2340c72b0aa3f2fe6b6cccec761856f9
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-01-17 11:09:07 +0700

    Menambahkan model LoginResponse untuk menangani respon login dari API

--- app/src/main/java/com/example/projectakhir_039/data/models/LoginResponse.kt
+++ app/src/main/java/com/example/projectakhir_039/data/models/LoginResponse.kt
@@ -3,5 +3,6 @@ package com.example.projectakhir_039.data.models
 data class LoginResponse(
     val status: String,
     val message: String,
-    val role: String? = null
+    val role: String? = null,
+    val user: User? = null // Tambahkan ini untuk menangkap objek user dari PHP
 )
