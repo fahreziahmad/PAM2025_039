d78203a8dcca823cec5b3c57b106c22ab7ee27ec
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-01-17 15:56:27 +0700

    implementasi OrderViewModel dengan StateFlow untuk manajemen pesanan dan item

--- app/src/main/java/com/example/projectakhir_039/viewmodel/OrderViewModel.kt
+++ app/src/main/java/com/example/projectakhir_039/viewmodel/OrderViewModel.kt
@@ -1,2 +1,24 @@
 package com.example.projectakhir_039.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.projectakhir_039.data.api.ApiClient
+import com.example.projectakhir_039.data.models.Order
+import com.example.projectakhir_039.data.models.OrderItem
+import com.example.projectakhir_039.data.repository.OrderRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class OrderViewModel : ViewModel() {
+    private val repository = OrderRepository()
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+    private val _orders = MutableStateFlow<List<Order>>(emptyList())
+    val orders: StateFlow<List<Order>> = _orders
+    private val _orderItems = MutableStateFlow<List<OrderItem>>(emptyList())
+    val orderItems: StateFlow<List<OrderItem>> = _orderItems
+    private val _allOrders = MutableStateFlow<List<Order>>(emptyList())
+    val allOrders: StateFlow<List<Order>> = _allOrders
