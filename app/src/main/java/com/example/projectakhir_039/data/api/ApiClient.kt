15c921fd349ad2ce8826edbc976c71d5c68b365e
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-01-17 13:44:00 +0700

    mengganti ip address supaya bisa di jalankan

--- app/src/main/java/com/example/projectakhir_039/data/api/ApiClient.kt
+++ app/src/main/java/com/example/projectakhir_039/data/api/ApiClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://10.69.11.113/api/"
+    private const val BASE_URL = "http://192.168.1.12/api/"
     val apiService: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
