a7e17cdb42fdb5c36d9a52da65cae47c26c67b50
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-01-17 11:05:06 +0700

    Menambahkan data model CartItem untuk merepresentasikan item dalam keranjang belanja

--- app/src/main/java/com/example/projectakhir_039/data/models/CartItem.kt
+++ app/src/main/java/com/example/projectakhir_039/data/models/CartItem.kt
@@ -1,13 +1,11 @@
 package com.example.projectakhir_039.data.models
 
-
 data class CartItem(
     val id: Int,
     val productId: Int,
-    val productName: String,
+    val name: String, // Pastikan ini 'name'
     val price: Double,
     val quantity: Int,
-    val imageUrl: String,
-    val userId: Int,
-    val selectedSize: String = "40"
+    val imageResId: Int,
+    val userId: Int
 )
