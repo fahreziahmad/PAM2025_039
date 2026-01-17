f1cfe4dc5fb01c576805fb151594c5135d56cc7b
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-01-17 11:25:01 +0700

    Menambahkan data model Order untuk representasi data pesanan pengguna

--- app/src/main/java/com/example/projectakhir_039/data/models/Order.kt
+++ app/src/main/java/com/example/projectakhir_039/data/models/Order.kt
@@ -1,14 +1,13 @@
 package com.example.projectakhir_039.data.models
 
-
 data class Order(
-    val id: Int,
-    val userId: Int,
-    val orderNumber: String,
-    val totalPrice: Double,
-    val status: String = "pending", // pending, processed, shipped, completed
-    val shippingAddress: String,
-    val paymentMethod: String, // COD, Transfer
-    val items: List<CartItem>,
-    val createdAt: String = ""
+    val id: Int = 0,
+    val userId: Int = 0,
+    val totalPrice: Double = 0.0,
+    val status: String = "Pending",
+    val paymentMethod: String = "COD",
+    val createdAt: String = "",
+    val shippingAddress: String = "",
+    val items: List<CartItem> = emptyList(), // Sesuai permintaan compiler
+    val orderNumber: String = ""
 )
